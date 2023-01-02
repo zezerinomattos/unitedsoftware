c415fb2799406984edb624b05996dd9218dec408
zezerinodev <mattoszz@hotmail.com>
2023-01-02 14:17:28 -0300

    Colocando logo na aba do navegador

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // MY IMPORTS
 import './style.css';
+import './responsive.css';
 import Navbar from '../../components/Navbar';
 
 // IMAGES
-
+import imgZeze from '../../assets/HeaderZezerino.png'
 
 export default function Home(){
     return(
         <header className='container'>
             <Navbar />
+            <div className='header-inf-mestre'>
+                <div className='textos'>
+                    <h1>UNITED SOFTWARE</h1>
+                    <h2>Welcome to the Future!</h2>
+
+                    <Link to="#" >FAÇA UM ORÇAMENTO SEM COMPROMISSO</Link>
+                </div>
+                
+                <div className='img-ze'>
+                    <img src={imgZeze} alt="Imagem" />
+                </div>
+            </div>
         </header>
     );
 }
