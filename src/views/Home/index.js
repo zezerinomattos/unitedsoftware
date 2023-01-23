51fed0699271ccc6b4a486538c5495dd3b9d9d05
zezerinodev <mattoszz@hotmail.com>
2023-01-23 15:13:05 -0300

    Section paginas texto finalizada

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -41,6 +41,31 @@ export default function Home(){
                     </p>
                 </div>
             </section>
+
+            <section className='paginas'>
+                <div className='paginas-superior'>
+                    <h1>LAYOUT EXCLUSIVO PARA SUA EMPRESA</h1>
+                    <h3>EXPERIÊNCIAS MODERNAS, EFICIENTES, INOVADORAS E INTUITIVAS</h3>
+                    <div className='paginas-superior-texto'>
+                        <div className='paginas-superior-texto-esq'>
+                            <p>
+                                Você quer ver sua empresa, sua marca ou seu negócio crescer, ganhar corpo, se destacar entre as inúmeras concorrências? Então encaminhe-a para o futuro, aqui na United Software vamos cuidar para que seu negócio trafegue pelo mundo através dos maiores buscadores do planeta.
+                            </p>
+                            <p>
+                                O design da sua loja é essencial para o sucesso do seu negócio. A primeira coisa que seus possíveis clientes observarão ao chegarem ao seu portal é a aparência dele. Para isso o visual precisa atraí-los, caso contrário, nem se darão ao trabalho de olhar os seus produtos.
+                            </p>
+                        </div>
+                        <div className='paginas-superior-texto-dir'>
+                            <p>
+                                Produzimos um trabalho moderno e altamente criativo o qual vai cumprir como papel principal de comercialização: a criação de receitas através de vendas e reconhecimento da marca no meio digital.
+                            </p>
+                            <p>
+                                Desenvolvemos o layout conforme as necessidades da sua empresa, público alvo e características baseado na sua linha de produtos e filosofia. Com nossa metodologia de prototipação nossos clientes acompanham todo processo criativo do seu material, podendo solicitar alterações até a sua aprovação.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
